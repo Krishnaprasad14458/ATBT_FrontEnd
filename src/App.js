215bd82771188d131c498d27013672d379c0a69f
irfan-shaikmd <155518181+irfan-shaikmd@users.noreply.github.com>
2024-01-06 16:24:55 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './App.css';
+import ChangePassword from './components/pages/auth/ChangePassword';
+import Login from './components/pages/auth/Login';
+import ResetPassword from './components/pages/auth/ResetPassword';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello ATBT</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
