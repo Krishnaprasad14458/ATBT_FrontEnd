fae12995a54ffaec04410bbc810736e0639a480e
irfan-shaikmd <155518181+irfan-shaikmd@users.noreply.github.com>
2024-01-09 14:58:48 +0530

    feat: init reports

---
+++ src/components/common/charts/UserChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserChart() {
+  return (
+    <div>UserChart</div>
+  )
+}
+
+export default UserChart
---
+++ src/components/pages/reports/Reports.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserChart from '../../common/charts/UserChart'
+
+function Reports() {
+  return (
+    <div>
+        <UserChart/>
+    </div>
+  )
+}
+
+export default Reports
