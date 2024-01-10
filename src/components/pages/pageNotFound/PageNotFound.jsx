a911bdd00bad8bfd1d4195b994b13789a0b96e7b
Lakshmi180 <152608571+Lakshmi180@users.noreply.github.com>
2024-01-10 10:21:20 +0530

    changes in folder stru

--- src/components/pages/pageNotFound/PageNotFound.jsx
+++ src/components/pages/pageNotFound/PageNotFound.jsx
@@ -1,16 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import './notFound.css';
-
+import { Link } from 'react-router-dom'
 const PageNotFound = () => {
   const navigate = useNavigate();
   document.title = "Page Not Found";
   return (
     <div className="not-found">
-      <div>
+      {/* <div>
         <h1>404</h1>
         <p>Page not found</p>
         <button onClick={() => navigate("/")}>Back to Home</button>
-      </div>
+      </div> */}
     </div>
   );
 };
---
+++ src/components/pages/task/Tasks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+function Tasks() {
+  return (
+    <div>
+      <h1>tasks</h1>
+    </div>
+  );
+}
+
+export default Tasks;
