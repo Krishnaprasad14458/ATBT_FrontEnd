0b2fb31246a019bf6fe9f34506b98ca658bf2080
Lakshmi180 <152608571+Lakshmi180@users.noreply.github.com>
2024-01-12 14:38:59 +0530

    fix:setting page and board meetinf form, task form, user form, entity form design added

--- src/components/createForm/createUserForm/UserForm.jsx
+++ src/components/createForm/createUserForm/UserForm.jsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 
 function UserForm() {
   return (
@@ -15,7 +8,7 @@ function UserForm() {
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
           <div class="col-span-1 p-3">
             <p className="text-2xl font-semibold">New User</p>
-            <form className="space-y-6 mt-4" method="POST">
+            <form className="space-y-4 mt-4" method="POST">
               <div>
                 <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
                 <div className="">
@@ -43,17 +36,19 @@ function UserForm() {
               <div className="relative">
                 <input type="file" id="fileInput" className="p-3 block w-full rounded-md border border-1 border-gray-400 py-1 bg-white text-gray-900 appearance-none shadow-sm  placeholder:text-gray-400 focus:outline-none focus:border-orange-400 sm:text-sm sm:leading-6" />
               </div>
-              <div class="relative inline-block text-left w-full">
-                <select class="block appearance-none p-3 w-full rounded-md border border-1 border-gray-400 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:outline-none focus:border-orange-400 sm:text-sm sm:leading-6">
-                  <option value="option1">Select Entity</option>
-                  <option value="option2">Option 1</option>
-                  <option value="option3">Option 2</option>
-                  <option value="option4">Option 3</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                  </svg>
+              <div>
+                <label htmlFor="designation" className="block text-sm font-medium leading-6 text-gray-900">Select Entity</label>
+                <div class="relative inline-block text-left w-full">
+                  <select class="block appearance-none p-3 w-full rounded-md border border-1 border-gray-400 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:outline-none focus:border-orange-400 sm:text-sm sm:leading-6">
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                  </div>
                 </div>
               </div>
               <div className=''>
