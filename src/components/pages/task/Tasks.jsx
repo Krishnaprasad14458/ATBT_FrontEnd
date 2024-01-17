f36c13da431b5f0ea5e533c465de9fa7776a3211
bhavithayadiki <139497954+bhavithayadiki@users.noreply.github.com>
2024-01-17 15:01:13 +0530

    tasks

--- src/components/pages/task/Tasks.jsx
+++ src/components/pages/task/Tasks.jsx
@@ -4,9 +4,7 @@ import { Link, Outlet } from 'react-router-dom';
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Link } from 'react-router-dom'
+
 import { ifStatement } from '@babel/types';
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -17,7 +15,7 @@ function Tasks() {
   return (
     <div className=' p-2 bg-[#f8fafc] h-screen'>
       <h1 className='m-3 font-semibold text-lg'>Board Metting</h1>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
