2062245b2286b1f96354857d32d988ce04796721
irfan-shaikmd <155518181+irfan-shaikmd@users.noreply.github.com>
2024-02-04 01:06:54 +0530

    feat: added layouts

--- src/components/layout/RequireAuth.jsx
+++ src/components/layout/RequireAuth.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../contexts/authContext/authContext";
-import { Navigate, useLocation, Outlet } from "react-router-dom";
+import { Navigate, useLocation, Routes, Outlet } from "react-router-dom";
+import Layout from "./SideAndTopBars";
 
 const RequireAuth = () => {
   const { authState } = useContext(AuthContext);
   const location = useLocation();
   return authState.token ? (
-    <Outlet />
+    <Layout>
+      <Outlet/>
+    </Layout>
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
   );
