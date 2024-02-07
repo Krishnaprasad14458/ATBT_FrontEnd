a32d690b8a435f612c22d7d3587eee2b88427978
bhavithayadiki <139497954+bhavithayadiki@users.noreply.github.com>
2024-02-07 10:28:44 +0530

    teamsdashboard

--- src/components/pages/dashboard/TeamsDashboard/TeamsDashboard.jsx
+++ src/components/pages/dashboard/TeamsDashboard/TeamsDashboard.jsx
@@ -131,6 +131,7 @@ const TeamsDashboard = () => {
                         Next
                     </a>
                 </div>
+                <div>Showing 1 to 5 of 5 Result</div>
                 {/*only for big screen pagination */}
                 <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
                     {/* pagination data */}
