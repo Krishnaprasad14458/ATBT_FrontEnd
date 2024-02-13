1fcc4d46c7d313119711763b92021974004041ea
bhavithayadiki <139497954+bhavithayadiki@users.noreply.github.com>
2024-02-13 15:02:20 +0530

    userFormdup

--- src/components/pages/settings/SettingsComponents/Forms/UsersFormDup.jsx
+++ src/components/pages/settings/SettingsComponents/Forms/UsersFormDup.jsx
@@ -217,25 +217,47 @@ const UsersFormDup = () => {
                                     <div className="flex flex-wrap mb-5 gap-3">
                                         {/* {input.type.charAt(0).toUpperCase() + input.type.slice(1)} */}
                                         <div className="w-1/5 hidden sm:block"></div>
-                                        <div class=" flex flex-wrap pt-5  gap-2">
-                                            <input
-                                                // className="mb-1"
+                                        <div class=" flex flex-wrap pt-5  gap-1 ">
+
+
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mt-1">
+                                                {input.mandatory ? (
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                ) : (
+
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+
+                                                )}
+
+                                            </svg>
+                                            {/* <input
+
                                                 type="checkbox"
                                                 id="mandatory"
                                                 name="mandatory"
                                                 checked={input.mandatory}
                                                 onChange={handleInputChange}
-                                            />
+                                            /> */}
                                             <div class=" text-body text-darkSlate01"> Required</div>
                                         </div>
-                                        <div class="  flex flex-wrap pt-5  gap-2 ">
-                                            <input
+                                        <div class="  flex flex-wrap pt-5  gap-1 ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mt-1">
+                                                {input.filterable ? (
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                ) : (
+
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+
+                                                )}
+
+                                            </svg>
+                                            {/* <input
                                                 type="checkbox"
                                                 id="filterable"
                                                 name="filterable"
                                                 checked={input.filterable}
                                                 onChange={handleInputChange}
-                                            />
+                                            /> */}
                                             <div class=" text-body text-darkSlate01  lg:pe-20">Filtered</div>
                                         </div>
                                     </div>
