d282196c265122428e8618fb0dea3df1773742ee
irfan-shaikmd <155518181+irfan-shaikmd@users.noreply.github.com>
2024-03-07 03:12:17 +0530

    wip: rbac

--- src/router/entity/entity.router.js
+++ src/router/entity/entity.router.js
@@ -1,9 +1,22 @@
 import EntityForm from "../../components/createForm/createEntityForm/EntityForm";
 import EntityLandingPage from "../../components/landingPages/entity/EntityLandingPage";
 import Entities from "../../components/pages/entities/Entities";
+import RouteBlocker from "../../rbac/RouteBlocker";
 
 export const entityRouter = [
-    { path: 'entities', element: <Entities />, },
-    { path: 'entitylandingpage/:id', element: <EntityLandingPage /> },
-    { path: 'entities/new', element: <EntityForm /> },
+    {
+        element: <RouteBlocker permissionCheck={(permission) =>
+            permission.module === 'entity' && permission.canRead} />,
+        children: [
+            { path: 'entities', element: <Entities />, },
+            { path: 'entitylandingpage/:id', element: <EntityLandingPage /> },
+        ]
+    },
+    {
+        element: <RouteBlocker permissionCheck={(permission) =>
+            permission.module === 'entity' && permission.canCreate} />,
+        children: [
+            { path: 'entities/new', element: <EntityForm /> },
+        ]
+    },
 ]
