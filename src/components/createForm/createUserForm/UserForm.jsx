e320febb30d4474324702fd592227d9fa10e0eaf
irfan-shaikmd <155518181+irfan-shaikmd@users.noreply.github.com>
2024-03-06 18:28:50 +0530

    wip

--- src/components/createForm/createUserForm/UserForm.jsx
+++ src/components/createForm/createUserForm/UserForm.jsx
@@ -165,7 +165,7 @@ function UserForm() {
             ...prev,
             [customFormFields[i].inputname]:
               'Name should contain atleast 3 characters',
-          })); 
+          }));
 
           isErrorspresent = true;
         } else {
@@ -434,15 +434,14 @@ function UserForm() {
       if (!!id && !!user?.userData) {
         console.log('updating');
         response = await updateUser(formData, id);
+        console.log(response);
+        // response?.status === 200 && navigate(`/users/${response.data}`);
       } else {
         console.log('creating');
         response = await createUser(formData);
+        response?.status === 201 && navigate(`/users/${response.data}`);
       }
       console.log('jsonData submitted', response);
-      if (response.status === 201) {
-        console.log('data is 201');
-        navigate(`/users/${response.data}`);
-      }
     }
   }
 
@@ -474,7 +473,8 @@ function UserForm() {
                           htmlFor={item.label}
                           className='block text-sm font-medium leading-6 mt-2 text-gray-900'
                         >
-                          {item.label.charAt(0).toUpperCase() + item.label.slice(1)}
+                          {item.label.charAt(0).toUpperCase() +
+                            item.label.slice(1)}
                         </label>
                         <input
                           type='text'
@@ -487,12 +487,13 @@ function UserForm() {
                         />
                         <div className='h-2 text-red-500'>
                           {errors[item.inputname] && (
-                            <span className='text-xs'>{errors[item.inputname]}</span>
+                            <span className='text-xs'>
+                              {errors[item.inputname]}
+                            </span>
                           )}
                         </div>
                       </div>
-                    )
-                  }
+                    )}
 
                   {item.type === 'file' &&
                     item.inputname === 'image' &&
@@ -502,7 +503,8 @@ function UserForm() {
                           htmlFor={item.label}
                           className='block text-sm font-medium leading-6 mt-2 text-gray-900'
                         >
-                          {item.label.charAt(0).toUpperCase() + item.label.slice(1)}
+                          {item.label.charAt(0).toUpperCase() +
+                            item.label.slice(1)}
                         </label>
                         <input
                           type='file'
@@ -514,12 +516,13 @@ function UserForm() {
                         />
                         <div className='h-2 text-red-500'>
                           {errors[item.inputname] && (
-                            <span className='text-xs'>{errors[item.inputname]}</span>
+                            <span className='text-xs'>
+                              {errors[item.inputname]}
+                            </span>
                           )}
                         </div>
                       </div>
-                    )
-                  }
+                    )}
 
                   {item.type === 'select' &&
                     item.inputname === 'entityname' &&
@@ -529,7 +532,8 @@ function UserForm() {
                           htmlFor={item.label}
                           className='block text-sm font-medium leading-6 mt-2 text-gray-900'
                         >
-                          {item.label.charAt(0).toUpperCase() + item.label.slice(1)}
+                          {item.label.charAt(0).toUpperCase() +
+                            item.label.slice(1)}
                         </label>
                         <select
                           id={item.inputname}
@@ -542,26 +546,31 @@ function UserForm() {
                             value=''
                             disabled
                             defaultValue
-
                           >
                             Please select
                           </option>
                           {item.options.value &&
                             fieldsDropDownData.entityname &&
-                            fieldsDropDownData.entityname.map((option, index) => (
-                              <option key={index} value={option}>
-                                {option}
-                              </option>
-                            ))}
+                            fieldsDropDownData.entityname.map(
+                              (option, index) => (
+                                <option
+                                  key={index}
+                                  value={option}
+                                >
+                                  {option}
+                                </option>
+                              )
+                            )}
                         </select>
                         <div className='h-2 text-red-500'>
                           {errors[item.inputname] && (
-                            <span className='text-xs'>{errors[item.inputname]}</span>
+                            <span className='text-xs'>
+                              {errors[item.inputname]}
+                            </span>
                           )}
                         </div>
                       </div>
-                    )
-                  }
+                    )}
 
                   {item.type === 'email' &&
                     item.inputname == 'email' &&
@@ -1446,7 +1455,7 @@ function UserForm() {
                         )}
                       </div>
                     )}
-                    
+
                     {item.type === 'range' && item.field == 'custom' && (
                       <div className='my-2 ms-5'>
                         {item.value && item.value.length > 0 && (
