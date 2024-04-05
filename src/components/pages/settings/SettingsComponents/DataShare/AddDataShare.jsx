72663adfc47ba7e1ba7d30643bac2ebe1086203f
bhavithayadiki <bhavitha.y@teksacademy.com>
2024-04-05 17:33:59 +0530

    adddatashare

--- src/components/pages/settings/SettingsComponents/DataShare/AddDataShare.jsx
+++ src/components/pages/settings/SettingsComponents/DataShare/AddDataShare.jsx
@@ -21,9 +21,9 @@ const AddDataShare = () => {
   console.log("selectedOptions", selectedOptions);
   return (
     <div className=" p-4 bg-[#f8fafc]">
-      <div className="grid grid-cols-2  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-5 ">
-        <div className="col-span-1 ">
-          <label className=" block text-sm font-medium leading-6 mt-2 text-gray-900 mb-1">
+      <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 lg:gap-5 gap-y-4  ">
+        <div className="col-span-1">
+          <label className=" block text-sm font-medium leading-6  text-gray-900 mb-1">
             Name
           </label>
           <input
@@ -32,8 +32,8 @@ const AddDataShare = () => {
             className="px-2 py-2 text-sm block w-full rounded-md bg-gray-50 border border-gray-300 text-gray-900 focus:outline-none focus:border-orange-400 placeholder:text-xs"
           />
         </div>
-        <div className="col-span-1 ">
-          <label className=" block text-sm font-medium leading-6 mt-2 text-gray-900 mb-1">
+        <div className="col-span-1">
+          <label className=" block text-sm font-medium leading-6  text-gray-900 mb-1">
             Description
           </label>
           <input
@@ -43,16 +43,16 @@ const AddDataShare = () => {
           />
         </div>
       </div>
-      <div className="grid grid-cols-2  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-5 mt-2 ">
+      <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-5 mt-2 ">
         <div className="col-span-1 ">
-          <div className="grid grid-cols-3  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-5 ">
-            <div className="col-span-1">
+          <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 md:gap-5 gap-y-5  ">
+           
               <label className=" block text-sm font-medium leading-6 mt-2 text-gray-900 mb-1">
                 Share data of
               </label>
-            </div>
+       
           </div>
-          <div className="grid grid-cols-3  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-5 ">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 lg:gap-5 gap-y-5">
             <div className="col-span-1">
               <Select
                 options={userOptions}
@@ -116,14 +116,14 @@ const AddDataShare = () => {
           </div>
         </div>
         <div className="col-span-1 ">
-          <div className="grid grid-cols-3  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-5 ">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 lg:gap-5 gap-y-5 ">
             <div className="col-span-1">
-              <label className=" block text-sm font-medium leading-6 mt-2 text-gray-900 mb-1">
+              <label className=" block text-sm font-medium leading-6 md:mt-2 text-gray-900 mb-1">
                 Share data with
               </label>
             </div>
           </div>
-          <div className="grid grid-cols-3  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-5 ">
+          <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 lg:gap-5 gap-y-5">
             <div className="col-span-1">
               <div className="px-2 py-1.5  block w-full rounded-md bg-gray-50 border border-gray-300 text-gray-900 focus:outline-none focus:border-orange-400">
                 <span className="text-sm"> User</span>
