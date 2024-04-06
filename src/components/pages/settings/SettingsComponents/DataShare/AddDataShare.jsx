405a2ec5036fa715a6a94211480784771df7b20f
Irshad3103 <146926779+Irshad3103@users.noreply.github.com>
2024-04-06 12:31:57 +0530

    Update AddDataShare.jsx

--- src/components/pages/settings/SettingsComponents/DataShare/AddDataShare.jsx
+++ src/components/pages/settings/SettingsComponents/DataShare/AddDataShare.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Select from "react-select";
 import atbtApi from '../../../../../serviceLayer/interceptor';
 
@@ -23,15 +23,65 @@ export async function loader({ request, params }) {
 }
 const AddDataShare = () => {
     const data = useLoaderData();
-    console.log("dataa", data)
-    const [selectedOptions, setSelectedOptions] = useState([]);
-    let userOptions = data.users
-    let entityOptions = data.entities
+    let moduleOptions = [{ value: "user", label: "user" }, { value: "entity", label: "entity" }]
+    const [module, setModule] = useState("");
+    const handleModuleChange = (selected) => {
+        setModule(selected)
+    }
+    const [shareDataofOptions, setShareDataOfOptions] = useState([])
+    useEffect(() => {
+        if (module?.value === "user") {
+            setShareDataOfOptions(data.users)
+            setShareDataOfSelectedOptions([])
+        }
+        else if (module?.value === "entity") {
+            setShareDataOfOptions(data.entities)
+            setShareDataOfSelectedOptions([])
+        }
+    }, [module])
+    const [shareDataOfSelectedOptions, setShareDataOfSelectedOptions] = useState([])
 
-    const handleSelectChange = (selected) => {
-        setSelectedOptions(selected);
+    const handleShareDataOf = (selected) => {
+        setShareDataOfSelectedOptions(selected);
     };
-    console.log("selectedOptions", selectedOptions);
+    const [shareDataWithOptions, setShareDataWithOptions] = useState(data.users)
+    const [shareDataWithSelectedOptions, setShareDataWithSelectedOptions] = useState({})
+    const handleShareDataWith = (selected) => {
+        setShareDataWithSelectedOptions(selected);
+    };
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleInputChange = (newValue) => {
+        setSearchValue(newValue);
+    };
+    useEffect(() => {
+        if (module?.value === "user") {
+            let filteredData = data.users.filter((item) => !shareDataOfSelectedOptions.some((filterItem) => filterItem.value === item.value))
+            setShareDataWithOptions(filteredData)
+        }
+
+    }, [shareDataOfSelectedOptions])
+    useEffect(() => {
+        if (module?.value === "user") {
+            let filteredData = data.users.filter((item) => shareDataWithSelectedOptions.value !== item.value)
+            setShareDataOfOptions(filteredData)
+        }
+
+    }, [shareDataWithSelectedOptions])
+    const handleSubmit = async () => {
+        let moduleName = module.value
+        let shareDataOf = shareDataOfSelectedOptions.map((item) => item.value)
+        let shareDataWith = shareDataWithSelectedOptions.value
+        console.log("module", moduleName, "shareDataOf", shareDataOf, "shareDataWith", shareDataWith)
+        if (moduleName === "user") {
+            await atbtApi.post("access/selected", { selectedUsers: shareDataOf })
+        }
+        else if (moduleName === "entity") {
+            await atbtApi.post(`access/entity/${shareDataOf[0]}`)
+        }
+
+    }
+
     return (
         <div className=" p-4 bg-[#f8fafc]">
             <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 lg:gap-5 gap-y-4">
@@ -68,7 +118,7 @@ const AddDataShare = () => {
                     <div className="grid grid-cols-1  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 lg:gap-5 gap-y-5">
                         <div className="col-span-1">
                             <Select
-                                options={userOptions}
+                                options={moduleOptions}
                                 styles={{
                                     option: (provided, state) => ({
                                         ...provided,
@@ -91,6 +141,8 @@ const AddDataShare = () => {
                                         primary: "#fb923c",
                                     },
                                 })}
+                                value={module}
+                                onChange={handleModuleChange}
                             />
                         </div>
                         <div className="col-span-2">
@@ -119,11 +171,12 @@ const AddDataShare = () => {
                                 })}
                                 isMulti
                                 name="colors"
-                                options={userOptions}
+                                options={shareDataofOptions}
                                 className="basic-multi-select "
                                 classNamePrefix="select"
-                                value={selectedOptions}
-                                onChange={handleSelectChange}
+                                value={shareDataOfSelectedOptions}
+                                onChange={handleShareDataOf}
+                                onInputChange={handleInputChange}
                             />
                         </div>
                     </div>
@@ -144,7 +197,7 @@ const AddDataShare = () => {
                         </div>
                         <div className="col-span-2">
                             <Select
-                                options={userOptions}
+                                options={shareDataWithOptions}
                                 styles={{
                                     option: (provided, state) => ({
                                         ...provided,
@@ -167,13 +220,16 @@ const AddDataShare = () => {
                                         primary: "#fb923c",
                                     },
                                 })}
+                                value={shareDataWithSelectedOptions}
+                                onChange={handleShareDataWith}
                             />
                         </div>
                     </div>
                 </div>
             </div>
             <div className="flex justify-end ">
-                <button className="mt-4 px-3 py-2  whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-orange-600 text-primary-foreground shadow hover:bg-primary/90 shrink-0 text-white ">
+                <button className="mt-4 px-3 py-2  whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-orange-600 text-primary-foreground shadow hover:bg-primary/90 shrink-0 text-white "
+                    onClick={handleSubmit}>
                     Save
                 </button>
             </div>
