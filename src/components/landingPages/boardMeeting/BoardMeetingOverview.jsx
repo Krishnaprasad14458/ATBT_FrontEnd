b5ced187d86075ddb9636eab3b6b31cb03ca8c70
bhavithayadiki <bhavitha.y@teksacademy.com>
2024-04-12 12:54:06 +0530

    boardmeetingoverview

--- src/components/landingPages/boardMeeting/BoardMeetingOverview.jsx
+++ src/components/landingPages/boardMeeting/BoardMeetingOverview.jsx
@@ -124,12 +124,14 @@ const BoardMeetingOverview = () => {
           <path d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z" />
         </svg>
       </Link>
-      <button
+   <Link  to={`/boardmeetings/${id}/task`}>
+   <button
                 type="submit"
                 className=" flex  justify-center rounded-md bg-orange-600 px-3 py-2 text-sm font-medium leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
               >
                 Create Task
               </button>
+              </Link>
   
 </div>
       <div className="mt-3 flex justify-center">
