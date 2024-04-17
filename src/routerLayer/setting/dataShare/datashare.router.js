59f1e475843f5915ef080cf0de1914ebee8a8c80
Irshad3103 <146926779+Irshad3103@users.noreply.github.com>
2024-04-17 16:33:32 +0530

    wip

--- src/routerLayer/setting/dataShare/datashare.router.js
+++ src/routerLayer/setting/dataShare/datashare.router.js
@@ -1,10 +1,10 @@
 import AddDataShare, { loader as datashare } from "../../../components/pages/settings/SettingsComponents/DataShare/AddDataShare";
-import DataShare from "../../../components/pages/settings/SettingsComponents/DataShare/DataShare";
+import DataShare, { DataShareDeleteAction } from "../../../components/pages/settings/SettingsComponents/DataShare/DataShare";
 import { DataShareloader } from "../../../components/pages/settings/SettingsComponents/DataShare/DataShare";
 
 export const datasharerouter = [
     {
-        index: true,loader: DataShareloader, element: <DataShare />,
+        index: true,loader: DataShareloader, action:DataShareDeleteAction, element: <DataShare />,
     },
     { path: "adddatashare", loader: datashare, element: <AddDataShare /> }
 
