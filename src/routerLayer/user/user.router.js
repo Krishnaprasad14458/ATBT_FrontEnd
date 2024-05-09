2706dddb03437356f72b17903895e7cb72d3c3a5
Irshad3103 <146926779+Irshad3103@users.noreply.github.com>
2024-05-09 15:01:09 +0530

    wip : tasks

--- src/routerLayer/user/user.router.js
+++ src/routerLayer/user/user.router.js
@@ -45,7 +45,6 @@ export const userRouter = [
     path: ":id",
     children: [
       // /users/:id/ edit
-
       {
         path: "edit",
         loader: userFormLoader,
@@ -69,12 +68,8 @@ export const userRouter = [
             index: true,
             loader: userLandingLoader,
             element: <UserOverview />,
-            // handle: {
-            //   crumb: () => <Link to="">Overview</Link>,
-            // },
           },
           // /users/:id/tasks          all tasks
-
           {
             path: "tasks",
             loader: tasksLoader,
@@ -96,25 +91,17 @@ export const userRouter = [
             },
             children: [
               // /users/:id/:boardmeetings                     bmeetings list
-
               {
                 index: true,
                 loader: entityMeetingLoader,
                 action: entityMeetingAction,
                 element: <Boardmeeting />,
+                
               },
               // /users/:id/:boardmeetings/:BMid        tasks of :BMid  bmeetings
-
               {
                 path: ":BMid",
-                // loader: tasksLoader,
-                // action: TasksActions,
-                // element: <Tasks />,
-                // handle: {
-                //   crumb: (data) => (
-                //     <Link to={data.threadPath}>{data.threadName}</Link>
-                //   ),
-                // },
+               
                 children: [
                   {
                     index: true,
@@ -134,7 +121,6 @@ export const userRouter = [
                   },
                 ],
               },
-            
             ],
           },
           // /users/:id/documents                     documents
@@ -148,286 +134,5 @@ export const userRouter = [
         ],
       },
     ],
-    //     children :[
-    //   {index:true,  loader: userLandingLoader, element: <UserOverview />,},
-
-    // ]
-
-    // children: [
-    //   { path: "edit", loader: userFormLoader, element: <UserForm /> },
-    //   {
-    //     // index: true,
-    //     children: [
-    //       { path: "edit", loader: userFormLoader, element: <UserForm /> },
-    //       {
-    //         element: <UserLandingPage />,
-    //         children: [
-    //           {
-    //             index: true,
-    //             loader: userLandingLoader,
-    //             element: <UserOverview />,
-    //           },
-    //           {
-    //             path: "tasks",
-    //             element: <AllTasks />,
-    //           },
-    //           {
-    //             path: "boardmeetings",
-    //             children: [
-    //               {
-    //                 index: true,
-    //                 loader: entityMeetingLoader,
-    //                 action: entityMeetingAction,
-    //                 element: <Boardmeeting />,
-    //               },
-    //               {
-    //                 path: ":BMid",
-    //                 loader: tasksLoader,
-    //                 action: MeetingWiseTasksActions,
-    //                 element: <MeetingWiseTask />,
-    //               },
-    //             ],
-    //           },
-    //           {
-    //             path: "documents",
-    //             element: <Documents />,
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // ],
   },
 ];
-
-// children: [
-//   {
-//     index: true,
-//     loader: userLandingLoader,
-//     element: <UserOverview />,
-
-//   },
-//   {
-//     path: "tasks",
-//     element: <AllTasks />,
-
-//   },
-//   {
-//     path: ":moduleName/boardmeetings",
-//     loader: entityMeetingLoader,
-//     action: entityMeetingAction,
-//     element: <Boardmeeting />,
-
-//   },
-//   { path: "documents", element: <Documents /> ,
-
-// },
-// ],
-
-/////////////////////////////////
-
-// import UserForm, {
-//   userFormLoader,
-// } from "../../componentLayer/pages/users/createUserForm/UserForm";
-// import { Link } from "react-router-dom";
-// import Boardmeeting, {
-//   action as entityMeetingAction,
-//   loader as entityMeetingLoader,
-// } from "../../componentLayer/components/LandingPageComponents/Boardmeeting";
-// import Documents from "../../componentLayer/components/LandingPageComponents/Documents";
-// import { userLandingLoader } from "../../componentLayer/pages/users/userLandingPage/UserOverview";
-// import UserLandingPage from "../../componentLayer/pages/users/userLandingPage/UserLandingPage";
-// import UserOverview from "../../componentLayer/pages/users/userLandingPage/UserOverview";
-// import Users, {
-//   action as userAction,
-//   loader as userLoader,
-// } from "../../componentLayer/pages/users/usersList/Users";
-// import Tasks, {
-//   TasksActions,
-//   tasksLoader,
-// } from "../../componentLayer/components/LandingPageComponents/Tasks";
-
-// export const userRouter = [
-//       //   /users     <users/>
-//   {
-//     index: true,
-//     loader: userLoader,
-//     action: userAction,
-//     element: <Users />,
-//   },
-//     // /users/new
-
-//   {
-//     path: "new",
-//     loader: userFormLoader,
-
-//     element: <UserForm />,
-//     handle: {
-//       crumb: () => <Link to="/users/new">New User</Link>,
-//     },
-//   },
-//     // /users/:id
-
-//   {
-//     path: ":id",
-//     children: [
-//         // /users/:id/ edit
-
-//       {
-//         path: "edit",
-//         loader: userFormLoader,
-//         element: <UserForm />,
-//         handle: {
-//           crumb: (data) => <Link to={data.threadPath}>{data.threadName}</Link>,
-//         },
-//       },
-//       {
-//         element: <UserLandingPage />,
-//         loader: userLandingLoader,
-//         handle: {
-//           crumb: (data) => (
-//             <Link to={data?.data?.threadPath}>{data?.data?.threadName}</Link>
-//           ),
-//         },
-//         children: [
-//             // /users/:id          overview
-
-//           {
-//             index: true,
-//             loader: userLandingLoader,
-//             element: <UserOverview />,
-//             // handle: {
-//             //   crumb: () => <Link to="">Overview</Link>,
-//             // },
-//           },
-//               // /users/:id/tasks          all tasks
-
-//           {
-//             path: "tasks",
-//             loader: tasksLoader,
-//             action: TasksActions,
-//             element: <Tasks />,
-//             handle: {
-//               crumb: () => <Link to="">Tasks</Link>,
-//             },
-//           },
-//         // /users/:id/:boardmeetings                     bmeetings
-
-//           {
-//             path: ":boardmeetings",
-//             loader: entityMeetingLoader,
-//             handle: {
-//               crumb: (data) => (
-//                 <Link to={data.threadPath}>{data.threadName}</Link>
-//               ),
-//             },
-//             children: [
-//         // /users/:id/:boardmeetings                     bmeetings list
-
-//               {
-//                 index: true,
-//                 loader: entityMeetingLoader,
-//                 action: entityMeetingAction,
-//                 element: <Boardmeeting />,
-//               },
-//                    // /users/:id/:boardmeetings/:BMid        tasks of :BMid  bmeetings
-
-//               {
-//                 path: ":BMid",
-//                 loader: tasksLoader,
-//                 action: TasksActions,
-//                 element: <Tasks />,
-//                 handle: {
-//                   crumb: (data) => (
-//                     <Link to={data.threadPath}>{data.threadName}</Link>
-//                   ),
-//                 },
-//               },
-//             ],
-//           },
-//         // /users/:id/documents                     documents
-//           {
-//             path: "documents",
-//             element: <Documents />,
-//             handle: {
-//               crumb: () => <Link to="">Documents</Link>,
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//     //     children :[
-//     //   {index:true,  loader: userLandingLoader, element: <UserOverview />,},
-
-//     // ]
-
-//     // children: [
-//     //   { path: "edit", loader: userFormLoader, element: <UserForm /> },
-//     //   {
-//     //     // index: true,
-//     //     children: [
-//     //       { path: "edit", loader: userFormLoader, element: <UserForm /> },
-//     //       {
-//     //         element: <UserLandingPage />,
-//     //         children: [
-//     //           {
-//     //             index: true,
-//     //             loader: userLandingLoader,
-//     //             element: <UserOverview />,
-//     //           },
-//     //           {
-//     //             path: "tasks",
-//     //             element: <AllTasks />,
-//     //           },
-//     //           {
-//     //             path: "boardmeetings",
-//     //             children: [
-//     //               {
-//     //                 index: true,
-//     //                 loader: entityMeetingLoader,
-//     //                 action: entityMeetingAction,
-//     //                 element: <Boardmeeting />,
-//     //               },
-//     //               {
-//     //                 path: ":BMid",
-//     //                 loader: tasksLoader,
-//     //                 action: MeetingWiseTasksActions,
-//     //                 element: <MeetingWiseTask />,
-//     //               },
-//     //             ],
-//     //           },
-//     //           {
-//     //             path: "documents",
-//     //             element: <Documents />,
-//     //           },
-//     //         ],
-//     //       },
-//     //     ],
-//     //   },
-//     // ],
-//   },
-// ];
-
-// // children: [
-// //   {
-// //     index: true,
-// //     loader: userLandingLoader,
-// //     element: <UserOverview />,
-
-// //   },
-// //   {
-// //     path: "tasks",
-// //     element: <AllTasks />,
-
-// //   },
-// //   {
-// //     path: ":moduleName/boardmeetings",
-// //     loader: entityMeetingLoader,
-// //     action: entityMeetingAction,
-// //     element: <Boardmeeting />,
-
-// //   },
-// //   { path: "documents", element: <Documents /> ,
-
-// // },
-// // ],
