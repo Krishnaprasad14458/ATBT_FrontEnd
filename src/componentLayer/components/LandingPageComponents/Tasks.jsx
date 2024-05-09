0672927582975357d107e721a0e4c9bf12428b2a
Irshad3103 <146926779+Irshad3103@users.noreply.github.com>
2024-05-09 15:18:21 +0530

    Update Tasks.jsx

--- src/componentLayer/components/LandingPageComponents/Tasks.jsx
+++ src/componentLayer/components/LandingPageComponents/Tasks.jsx
@@ -191,7 +191,7 @@ const Tasks = () => {
   }, [data]);
 
   let fetcher = useFetcher();
-  const { id, BMid } = useParams();
+  const { id, BMid} = useParams();
   const [Qparams, setQParams] = useState({
     //  taskID:null
   });
@@ -334,30 +334,20 @@ const Tasks = () => {
   return (
     <div className="">
       <div className="flex justify-end">
-        {BMid && (
-          <button
-            className=" ms-2  mt-1 inline-flex items-center  whitespace-nowrap rounded-2xl text-sm font-medium  transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  text-orange-foreground shadow hover:bg-orange/90 h-9 px-3 py-1 shrink-0 bg-orange-600 text-white gap-1"
-            onClick={handleAddNewTask}
+       {BMid && <button
+          className=" ms-2  mt-1 inline-flex items-center  whitespace-nowrap rounded-2xl text-sm font-medium  transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  text-orange-foreground shadow hover:bg-orange/90 h-9 px-3 py-1 shrink-0 bg-orange-600 text-white gap-1"
+          onClick={handleAddNewTask}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5"
-            >
-              <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-            </svg>
-            Add Task
-          </button>
-        )}
-      </div>
-      <div>
-        <span>active </p>
-        <p>to do  </p>
-        <p>Overdue </p>
-        <p>completed </p>
-        <p>master </p>
-
+            <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+          </svg>
+          Add Task
+        </button>}
       </div>
       <div className=" mt-2 overflow-x-auto">
         <table className="w-full divide-y divide-gray-200 dark:divide-gray-700 rounded-md">
