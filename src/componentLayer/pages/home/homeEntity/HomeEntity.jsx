953ea6991b33f790bc0b7302c3c039bb9974c8d1
bhavi2001 <bhavitha.y@teksacademy.com>
2024-06-04 14:33:42 +0530

    home ui chnages

--- src/componentLayer/pages/home/homeEntity/HomeEntity.jsx
+++ src/componentLayer/pages/home/homeEntity/HomeEntity.jsx
@@ -130,13 +130,13 @@ function HomeEntity() {
         <div className="flex flex-1 justify-between sm:hidden">
           <a
             href="#"
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            className="relative inline-flex items-center rounded-md border border-gray-300 bg-[#f8fafc] px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
           >
             Previous
           </a>
           <a
             href="#"
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-[#f8fafc] px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
           >
             Next
           </a>
