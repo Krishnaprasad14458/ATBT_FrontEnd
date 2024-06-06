28440df38d1fe4857a3fe784e7f846d1eb998c0c
Irshad3103 <146926779+Irshad3103@users.noreply.github.com>
2024-06-06 16:39:47 +0530

    wip : changes

--- src/componentLayer/components/LandingPageComponents/Boardmeeting.jsx
+++ src/componentLayer/components/LandingPageComponents/Boardmeeting.jsx
@@ -360,7 +360,7 @@ function Boardmeeting() {
                         return (
                           <td
                             key={key}
-                            className={`px-3 py-2 text-left border border-[#e5e7eb] text-xs font-medium  overflow-hidden hover:text-orange-500`}
+                            className={`px-3 py-2 text-left border border-[#e5e7eb] text-xs font-medium  overflow-hidden `}
                             style={{ maxWidth: "160px" }}
                             title={row[key]}
                           >
