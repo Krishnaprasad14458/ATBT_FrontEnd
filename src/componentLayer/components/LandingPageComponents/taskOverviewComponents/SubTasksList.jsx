03fbf595722684b9ccda5c30384032e9b6a1b3c0
bhavi2001 <bhavitha.y@teksacademy.com>
2024-06-07 12:46:59 +0530

    Update SubTasksList.jsx

--- src/componentLayer/components/LandingPageComponents/taskOverviewComponents/SubTasksList.jsx
+++ src/componentLayer/components/LandingPageComponents/taskOverviewComponents/SubTasksList.jsx
@@ -49,7 +49,7 @@ const SubTasksList = ({
           >
             <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
           </svg>
-          Add Subtask
+          Create Subtask
         </button>
       </div>
    
@@ -139,7 +139,7 @@ const SubTasksList = ({
                     </td>
                     <td
                       className="border py-1.5 px-2"
-                      title={task?.members}
+                     
                       style={{width:"12rem"}}
                     >
                       <Select
