aeb5998a5ef11aaf50c5f0c26237b62e3ccd9c3e
bhavi2001 <bhavitha.y@teksacademy.com>
2024-06-11 16:22:25 +0530

    wip: tasks, boardmeeting

--- src/componentLayer/components/LandingPageComponents/Tasks.jsx
+++ src/componentLayer/components/LandingPageComponents/Tasks.jsx
@@ -323,7 +323,6 @@ export async function TasksActions({ request, params }) {
   }
 }
 const Tasks = () => {
-  
   const { authState } = useContext(AuthContext);
   console.log("authState authState", authState?.user?.id);
   let submit = useSubmit();
@@ -619,7 +618,20 @@ const Tasks = () => {
                   setDueDateFilter({ toDate: "", fromDate: "" });
                 }}
               >
-                Reset Date
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+                  />
+                </svg>
               </button>
 
               <TasksFilter Qparams={Qparams} setQParams={setQParams} />
