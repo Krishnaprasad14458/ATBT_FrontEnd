7e02d2de531d2b4c27b6dc338691c3d9f08386cb
bhavi2001 <bhavitha.y@teksacademy.com>
2024-06-12 10:33:30 +0530

    Update BoardMeetings.jsx

--- src/componentLayer/pages/boardmeetings/boardMeetingsList/BoardMeetings.jsx
+++ src/componentLayer/pages/boardmeetings/boardMeetingsList/BoardMeetings.jsx
@@ -138,6 +138,12 @@ function BoardMeetings() {
     );
     setvisibleColumns(visibleColumns);
   }, [tableView]);
+
+
+  const [filterMeetingDate , setFilterMeetingDate] = useState({
+    fromDate :"",
+    toDate:""
+  })
   return (
     <div className="overflow-x-auto p-3">
       {/* search & filter */}
@@ -186,51 +192,52 @@ function BoardMeetings() {
             <input
               className=" border border-gray-200  text-black px-1.5 py-2 rounded-md  bg-[#f9fafb] focus:outline-none text-sm focus:border-orange-400  date_type w-full"
               type="date"
-              // value={dueDateFilter.fromDate}
+              value={filterMeetingDate.fromDate}
               style={{
                 fontSize: "0.8rem",
                 WebkitAppearance: "none",
               }}
-              // onChange={(e) => {
+
+              onChange={(e) => {
                
-              //   setQParams((prev) => ({ ...prev, fromDate: e.target.value }));
-              //   setDueDateFilter((prev) => ({
-              //     ...prev,
-              //     fromDate: e.target.value,
-              //   }));
+                setQParams((prev) => ({ ...prev, fromDate: e.target.value }));
+                setFilterMeetingDate((prev) => ({
+                  ...prev,
+                  fromDate: e.target.value,
+                }));
         
-              // }}
+              }}
             />
             <label className="text-sm text-gray-400"> To:</label>
             <input
               className=" border border-gray-200 text-black px-1.5 py-2 rounded-md  bg-[#f9fafb] focus:outline-none text-sm focus:border-orange-400 date_type w-full"
               type="date"
-              // value={dueDateFilter.toDate}
+              value={filterMeetingDate.toDate}
 
               style={{
                 fontSize: "0.8rem",
                 WebkitAppearance: "none",
               }}
-              // onChange={(e) => {
-              //   setQParams((prev) => ({
-              //     ...prev,
+              onChange={(e) => {
+                setQParams((prev) => ({
+                  ...prev,
 
-              //     toDate: e.target.value,
-              //   }));
-              //   setDueDateFilter((prev) => ({
-              //     ...prev,
-              //     toDate: e.target.value,
-              //   }));
-              // }}
+                  toDate: e.target.value,
+                }));
+                setFilterMeetingDate((prev) => ({
+                  ...prev,
+                  toDate: e.target.value,
+                }));
+              }}
             />
             <button
-//              onClick={()=>{
-//               let Qprms = {...Qparams}
-// delete Qprms.fromDate
-// delete Qprms.toDate
-// setQParams(Qprms)
-// setDueDateFilter({toDate:"",fromDate:""})
-//             }}
+             onClick={()=>{
+              let Qprms = {...Qparams}
+delete Qprms.fromDate
+delete Qprms.toDate
+setQParams(Qprms)
+setFilterMeetingDate({toDate:"",fromDate:""})
+            }}
             ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
           </svg>
