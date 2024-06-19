e3fa90ff35b42b4f2043dd15ad101e8dd24b836a
bhavi2001 <bhavitha.y@teksacademy.com>
2024-06-19 18:26:57 +0530

    wip: homeenetity, homeuser, index.css

--- src/componentLayer/pages/home/homeUser/HomeUser.jsx
+++ src/componentLayer/pages/home/homeUser/HomeUser.jsx
@@ -136,18 +136,44 @@ function HomeUser() {
       <div className="flex items-center justify-between  px-4 py-3  sm:px-6 absolute inset-x-0 right-0 bottom-0">
         {/* hidden dashboard only for mobile */}
         <div className="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-[#f8fafc] px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
+        <button
+                disabled={
+                  fetcher?.state === "loading"
+                    ? true
+                    : false || data?.currentPage === 1
+                }
+                onClick={() => handlePage(data.currentPage - 1)}
+                className={`relative  inline-flex items-center rounded-md border border-gray-300 bg-[#f8fafc] px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 ${
+                  fetcher?.state === "loading"
+                    ? "cursor-wait"
+                    : data?.currentPage === 1
+                    ? "cursor-not-allowed"
+                    : "cursor-pointer"
+                }`}
+              >
             Previous
-          </a>
-          <a
+          </button>
+          <button
+                disabled={
+                  fetcher?.state === "loading"
+                    ? true
+                    : false || data?.currentPage === data?.totalPages
+                }
+                onClick={() => handlePage(data.currentPage + 1)}
+                className={`relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-[#f8fafc] px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 ${
+                  fetcher?.state === "loading"
+                    ? "cursor-wait"
+                    : data?.currentPage === data?.totalPages
+                    ? "cursor-not-allowed"
+                    : "cursor-pointer"
+                }`}
+              >
+          {/* <a
             href="#"
             className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-[#f8fafc] px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
+          > */}
             Next
-          </a>
+          </button>
         </div>
         {/*only for big screen dashboard */}
         <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
