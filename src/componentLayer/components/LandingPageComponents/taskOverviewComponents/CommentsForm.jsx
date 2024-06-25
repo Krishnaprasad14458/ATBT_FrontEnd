069c38f3b31c087a4250f2268b2bab34abc11fec
Irshad3103 <146926779+Irshad3103@users.noreply.github.com>
2024-06-25 18:13:43 +0530

    wip : changes

--- src/componentLayer/components/LandingPageComponents/taskOverviewComponents/CommentsForm.jsx
+++ src/componentLayer/components/LandingPageComponents/taskOverviewComponents/CommentsForm.jsx
@@ -13,17 +13,18 @@ const CommentsForm = ({
   isCommentEditing,
   setIsCommentEditing,
   fileName,
-  setFileName
+  setFileName,
 }) => {
   let fetcher = useFetcher();
-console.log("newComment",newComment)
+  console.log("newComment", newComment);
   const { authState } = useContext(AuthContext);
   const [file, setFile] = useState(null);
   console.log("file", file);
- 
+let [uploadStatus,setUploadStatus] = useState()
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isCommentEditing) {
+      setUploadStatus("Sending...")
       let postComment = newComment;
       postComment.senderId = authState?.user?.id;
       let response;
@@ -40,8 +41,10 @@ console.log("newComment",newComment)
             headers: { "Custom-Header": "value" },
           });
           if (response.status === 201) {
-            setFile(null)
-            setFileName(null)
+      setUploadStatus("")
+
+            setFile(null);
+            setFileName(null);
             // const formData = new FormData();
             // console.log("UpdateData 4",response.data,postComment);
             // formData.set("file", response.data);
@@ -79,34 +82,38 @@ console.log("newComment",newComment)
           console.error("Error during file upload:", error);
           // setMsg("Error In Uploading File");
         }
-      }else if(!file){  
-          let CommentData = {
-            file: null,
-            message: postComment.message,
-            senderId: postComment.senderId,
-          };
-          let UpdateData = {
-            id: taskID,
-            data: CommentData,
-            type: displayOverviewTask
-              ? "ADD_TASK_COMMENT"
-              : "ADD_SUBTASK_COMMENT",
-          };
-          console.log("UpdateData", UpdateData);
-          try {
-            fetcher.submit(UpdateData, {
-              method: "POST",
-              encType: "application/json",
-            });
-            setTimeout(() => {
-              scrollToBottom();
-            }, 1000);
-            setNewComment({ message: "",  senderId: "" });
-          } catch (error) {
-            console.log(error, "which error");
-          }
+      } else if (!file) {
+        let CommentData = {
+          file: null,
+          message: postComment.message,
+          senderId: postComment.senderId,
+        };
+        let UpdateData = {
+          id: taskID,
+          data: CommentData,
+          type: displayOverviewTask
+            ? "ADD_TASK_COMMENT"
+            : "ADD_SUBTASK_COMMENT",
+        };
+        console.log("UpdateData", UpdateData);
+        try {
+          fetcher.submit(UpdateData, {
+            method: "POST",
+            encType: "application/json",
+          });
+          setTimeout(() => {
+            scrollToBottom();
+          }, 1000);
+      setUploadStatus("")
+
+          setNewComment({ message: "", senderId: "" });
+        } catch (error) {
+          console.log(error, "which error");
+        }
       }
     } else if (isCommentEditing) {
+      setUploadStatus("Sending...")
+
       let postComment = newComment;
       let UpdateData = {
         id: postComment.id,
@@ -118,19 +125,21 @@ console.log("newComment",newComment)
           method: "PATCH",
           encType: "application/json",
         });
-        setFileName(null)
+        setFileName(null);
         setIsCommentEditing(false);
-        setNewComment({ message: "",  senderId: "" });
+      setUploadStatus("")
+
+        setNewComment({ message: "", senderId: "" });
       } catch (error) {
         console.log(error, "which error");
       }
     }
   };
- 
+
   console.log("newcomment", newComment);
-  console.log("fileName",fileName,file)
+  console.log("fileName", fileName, file);
   return (
-    <div className="p-3 ">
+    <div className="px-3 pt-3 ">
       <form>
         <div className="grid grid-cols-11 md:grid-cols-11 lg:grid-cols-11 xl:grid-cols-11 justify-center gap-3">
           <div className="col-span-10  flex items-end border-2  border-back rounded-md h-15">
@@ -145,20 +154,22 @@ console.log("newComment",newComment)
             <div>
               {fileName && <span>{fileName}</span>}
               <label htmlFor="fileInput" className="cursor-pointer">
-                {!isCommentEditing && <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5 me-2 mb-2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
-                  />
-                </svg>}
+                {!isCommentEditing && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 me-2 mb-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                    />
+                  </svg>
+                )}
               </label>
               <input
                 name="image"
@@ -166,10 +177,14 @@ console.log("newComment",newComment)
                 type="file"
                 className="hidden"
                 // onChange={handleFileChange}
-                onChange={(e) => {setFile(e.target.files[0]);setFileName(e.target.files[0].name)}}
+                onChange={(e) => {
+                  setFile(e.target.files[0]);
+                  setFileName(e.target.files[0].name);
+                }}
               />
             </div>
           </div>
+
           <div className="col-span-1 flex justify-center items-center">
             <button
               // type="submit"
@@ -198,6 +213,7 @@ console.log("newComment",newComment)
             </button>
           </div>
         </div>
+        <span className="flex justify-end text-xs"> {uploadStatus}</span>
       </form>
     </div>
   );
