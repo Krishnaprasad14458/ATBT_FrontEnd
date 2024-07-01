0857abfe7082d880b4cf69a1857ade0c6a0e3621
bhavi2001 <bhavitha.y@teksacademy.com>
2024-07-01 14:57:48 +0530

    commetsview

--- src/componentLayer/components/LandingPageComponents/taskOverviewComponents/CommentsView.jsx
+++ src/componentLayer/components/LandingPageComponents/taskOverviewComponents/CommentsView.jsx
@@ -191,11 +191,13 @@ const CommentsView = ({
                             {commentCrudView === comment.id && (
                               <div
                                 ref={menuRef}
-                                class="absolute right-0  z-10 mt-2 w-28 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                
+                                class="absolute right-0 bottom-0   z-10 mt-2 w-28 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                 role="menu"
                                 aria-orientation="vertical"
                                 aria-labelledby="menu-button"
                                 tabindex="-1"
+                                
                               >
                                 <div class="py-1" role="none">
                                   <p
